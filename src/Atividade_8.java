cf3d8d34426fee987a2ae26daae713a5d61c4da5
rogeriofranca <rogeriofranca715@gmail.com>
2023-05-18 15:38:23 -0300

    Atividade_8.java

--- src/Atividade_8.java
+++ src/Atividade_8.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 8. Escreva um algoritmo que receba o nome, idade, sexo e salário fixo de um funcionário.
     Calcule e mostre o nome e salário líquido do funcionário de acordo com a tabela:
@@ -8,4 +10,37 @@
     M < 30 80,00
  */
 public class Atividade_8 {
-}
+    public static void main(String[] args){
+        int idade;
+        String nome, sexo;
+        double salario, salario_liquido;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("informe o nome: ");
+        nome = scanner.next();
+        System.out.println("informe a idade: ");
+        idade = scanner.nextInt();
+        System.out.println("informe o sexo: ");
+        sexo = scanner.next();
+        System.out.println("informe o salario: ");
+        salario = scanner.nextDouble();
+
+        if (sexo == "m" && idade >= 30){
+            salario_liquido = salario + 100;
+            System.out.println(salario_liquido);
+        }
+        else if (sexo == "m" && idade < 30){
+            salario_liquido = salario + 80;
+            System.out.println(salario_liquido);
+        }
+        else if (sexo == "f" && idade >= 30){
+            salario_liquido = salario + 200;
+            System.out.println(salario_liquido);
+        }
+        else if (sexo == "f" && idade < 30){
+            salario_liquido = salario + 50;
+            System.out.println(salario_liquido);}
+        else{
+            System.out.println("Nao teve direito ao abono");
+        }
+        }
+    }
